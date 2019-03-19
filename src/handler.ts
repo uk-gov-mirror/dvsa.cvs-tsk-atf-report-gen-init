d937c556ec0ebb8bcc008488980304096a06a97b
Tony Cozianu <acozianu@deloittece.com>
2019-03-19 15:44:05 +0200

    CVSB-1241 - ATF Report gen init

---
+++ src/handler.ts
@@ -0,0 +1,13 @@
+import {atfGenInit} from "./functions/atfGenInit";
+import {config as AWSConfig} from "aws-sdk";
+
+const isOffline: boolean = (!process.env.BRANCH || process.env.BRANCH === "local");
+
+if (isOffline) {
+    AWSConfig.credentials = {
+        accessKeyId: "offline",
+        secretAccessKey: "offline"
+    };
+}
+
+export {atfGenInit as handler};
